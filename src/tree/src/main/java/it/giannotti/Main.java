fede3b4b4ddfc7ac29d3ffa0ec7a8795e2be2f4e
lai3-4-4 <lai3-4-4@itismeucci.com>
2024-12-20 09:08:59 +0100

    tree assignment

--- src/tree/src/main/java/it/giannotti/Main.java
+++ src/tree/src/main/java/it/giannotti/Main.java
@@ -2,6 +2,21 @@ package it.giannotti;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Node<String> a = new Node<>("r");
+        a.addChild(0, new Node<>("A"));
+        a.addChild(0, new Node<>("B"));
+        a.addChild(1, new Node<>("C"));
+        a.addChild(1, new Node<>("D"));
+        a.addChild(4, new Node<>("E"));
+
+        System.out.println(a.getDepth());
+        System.out.println(a.countLeaves());
+        System.out.println(a.countNodes());
+
+        System.out.println("DFS:");
+        a.depthFistSearch();
+        System.out.println("\nBFS:");
+        a.breadthFirstSearch();
     }
 }
--- src/tree/src/main/java/it/giannotti/tree/Tree.java
+++
@@ -1,6 +0,0 @@
-package it.giannotti.tree;
-
-public class Tree<T> {
-
-    private Node<T> root;
-}
